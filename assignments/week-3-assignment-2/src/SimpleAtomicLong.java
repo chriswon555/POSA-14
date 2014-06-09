50a76e805606e9768b7322581147731b2f61edea
Chris <chriswon555@gmail.com>
2014-06-08 22:27:05 -0600

    HW 2

--- assignments/week-3-assignment-2/src/SimpleAtomicLong.java
+++ assignments/week-3-assignment-2/src/SimpleAtomicLong.java
@@ -1,35 +1,33 @@
-// Import the necessary Java synchronization and scheduling classes.
-
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.Lock;
 
 /**
  * @class SimpleAtomicLong
- *
+ * 
  * @brief This class implements a subset of the
  *        java.util.concurrent.atomic.SimpleAtomicLong class using a
  *        ReentrantReadWriteLock to illustrate how they work.
  */
-class SimpleAtomicLong
-{
+class SimpleAtomicLong {
     /**
      * The value that's manipulated atomically via the methods.
      */
     private long mValue;
-    
+
     /**
      * The ReentrantReadWriteLock used to serialize access to mValue.
      */
-    // TODO - replace the null with the appropriate initialization:
-    private ReentrantReadWriteLock mRWLock = null;
+
+    // TODO -- you fill in here by replacing the null with an
+    // initialization of ReentrantReadWriteLock.
+    private ReentrantReadWriteLock mRWLock = new ReentrantReadWriteLock();
 
     /**
      * Creates a new SimpleAtomicLong with the given initial value.
      */
-    public SimpleAtomicLong(long initialValue)
-    {
-        long value = 0;
-        // TODO - you fill in here
+    public SimpleAtomicLong(long initialValue) {
+        // TODO -- you fill in here
+        mValue = initialValue;
     }
 
     /**
@@ -37,59 +35,77 @@ class SimpleAtomicLong
      * 
      * @returns The current value
      */
-    public long get()
-    {
-        long value = 0;
-        // TODO - you fill in here, using a readLock()
+    public long get() {
+        long value;
+
+        // TODO -- you fill in here
+        mRWLock.readLock().lock();
+        value = mValue;
+        mRWLock.readLock().unlock();
         return value;
     }
 
     /**
      * @brief Atomically decrements by one the current value
-     *
+     * 
      * @returns the updated value
      */
-    public long decrementAndGet()
-    {
+    public long decrementAndGet() {
         long value = 0;
-        // TODO - you fill in here, using a writeLock()
+
+        // TODO -- you fill in here
+        mRWLock.writeLock().lock();
+        mValue--;
+        value = mValue;
+        mRWLock.writeLock().unlock();
         return value;
     }
 
     /**
      * @brief Atomically increments by one the current value
-     *
+     * 
      * @returns the previous value
      */
-    public long getAndIncrement()
-    {
+    public long getAndIncrement() {
         long value = 0;
-        // TODO - you fill in here, using a writeLock()
+
+        // TODO -- you fill in here
+        mRWLock.writeLock().lock();
+        value = mValue;
+        mValue++;
+        mRWLock.writeLock().unlock();
         return value;
     }
 
     /**
      * @brief Atomically decrements by one the current value
-     *
+     * 
      * @returns the previous value
      */
-    public long getAndDecrement()
-    {
+    public long getAndDecrement() {
         long value = 0;
-        // TODO - you fill in here, using a writeLock()
+
+        // TODO -- you fill in here
+        mRWLock.writeLock().lock();
+        value = mValue;
+        mValue--;
+        mRWLock.writeLock().unlock();
         return value;
     }
 
     /**
      * @brief Atomically increments by one the current value
-     *
+     * 
      * @returns the updated value
      */
-    public long incrementAndGet()
-    {
+    public long incrementAndGet() {
         long value = 0;
-        // TODO - you fill in here, using a writeLock()
+
+        // TODO -- you fill in here
+        mRWLock.writeLock().lock();
+        mValue++;
+        mRWLock.writeLock().unlock();
+        value = mValue;
         return value;
     }
 }
-
