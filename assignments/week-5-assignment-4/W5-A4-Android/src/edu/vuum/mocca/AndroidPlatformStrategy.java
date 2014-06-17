c049973e0b689670938305573618005862bf5d92
Chris <chriswon555@gmail.com>
2014-06-17 04:19:56 -0600

    HW week 5 done

--- assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
+++ assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
@@ -10,25 +10,21 @@ import android.util.Log;
 /**
  * @class AndroidPlatformStrategy
  * 
- * @brief Provides methods that define a platform-independent API for
- *        output data to Android UI thread and synchronizing on thread
- *        completion in the ping/pong game.  It plays the role of the
- *        "Concrete Strategy" in the Strategy pattern.
+ * @brief Provides methods that define a platform-independent API for output
+ *        data to Android UI thread and synchronizing on thread completion in
+ *        the ping/pong game. It plays the role of the "Concrete Strategy" in
+ *        the Strategy pattern.
  */
-public class AndroidPlatformStrategy extends PlatformStrategy
-{	
+public class AndroidPlatformStrategy extends PlatformStrategy {
     /** TextViewVariable. */
     private TextView mTextViewOutput;
-	
+
     /** Activity variable finds gui widgets by view. */
     private WeakReference<Activity> mActivity;
 
-    public AndroidPlatformStrategy(Object output,
-                                   final Object activityParam)
-    {
+    public AndroidPlatformStrategy(Object output, final Object activityParam) {
         /**
-         * A textview output which displays calculations and
-         * expression trees.
+         * A textview output which displays calculations and expression trees.
          */
         mTextViewOutput = (TextView) output;
 
@@ -37,46 +33,53 @@ public class AndroidPlatformStrategy extends PlatformStrategy
     }
 
     /**
-     * Latch to decrement each time a thread exits to control when the
-     * play() method returns.
+     * Latch to decrement each time a thread exits to control when the play()
+     * method returns.
      */
-    private static CountDownLatch mLatch = null;
+    private CountDownLatch mLatch = null;
 
     /** Do any initialization needed to start a new game. */
-    public void begin()
-    {
+    public void begin() {
         /** (Re)initialize the CountDownLatch. */
         // TODO - You fill in here.
+        mLatch = new CountDownLatch(NUMBER_OF_THREADS);
     }
 
     /** Print the outputString to the display. */
-    public void print(final String outputString)
-    {
-        /** 
-         * Create a Runnable that's posted to the UI looper thread
-         * and appends the outputString to a TextView. 
+    public void print(final String outputString) {
+        /**
+         * Create a Runnable that's posted to the UI looper thread and appends
+         * the outputString to a TextView.
          */
         // TODO - You fill in here.
+        ((Activity) mActivity.get()).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextViewOutput.append(outputString + "\n");
+            }
+        });
     }
 
     /** Indicate that a game thread has finished running. */
-    public void done()
-    {	
+    public void done() {
         // TODO - You fill in here.
+        mLatch.countDown();
     }
 
     /** Barrier that waits for all the game threads to finish. */
-    public void awaitDone()
-    {
+    public void awaitDone() {
         // TODO - You fill in here.
+        try {
+            mLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    /** 
-     * Error log formats the message and displays it for the
-     * debugging purposes.
+    /**
+     * Error log formats the message and displays it for the debugging purposes.
      */
-    public void errorLog(String javaFile, String errorMessage) 
-    {
-       Log.e(javaFile, errorMessage);
+    public void errorLog(String javaFile, String errorMessage) {
+        Log.e(javaFile, errorMessage);
     }
 }
